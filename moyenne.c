d9529c051e7cf33254332cef2e771d91164d6c48
Zo51a <zofia13@hotmail.co.uk>
2016-11-19 17:17:53 +0100

    Update moyenne.c

--- moyenne.c
+++ moyenne.c
@@ -2,7 +2,7 @@
 
 int main(){
   int i;
-  double moyenne;
+  double moyenne=0.0;
   double tab[11]={1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8,9.9,10.0,'\0'};
   for (i=0;i<10;i++){
     moyenne=moyenne+tab[i];
